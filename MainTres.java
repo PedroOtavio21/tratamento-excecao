b4139f1fec308b843f1e6a10efa1244916502211
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-07-19 14:06:06 -0300

    Adicionando Classe Main3 para realizacao de terceira task do projeto, alem de arquivos bin

---
+++ MainTres.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class MainTres {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scannr(System.in);
+
+        Robo roboNormal = new Robo("Azul");
+        Robo roboInteligente = new RoboInteligente("Verde");
+
+        int alimentoX = 0;
+        int alimentoY = 0;
+        boolean inputValido = false;
+
+        // while(!inputValido){
+        //     try{
+        //         System.out.println("Insira a posição do alimento, de acordo com a posição no eixo cartesiano (x,y)");
+        //         alimentoX = scanner.nextInt();
+        //         alimentoY = scanner.nextInt();
+        //         if(alimentoX < 0 || alimentoY < 0){
+        //             System.out.println("As coordenadas do alimento não podem ser menores que 0!");
+        //             scanner.nextLine();
+        //         } else {
+        //             inputValido = true;
+        //         }
+        //     } catch (InputMismatchException e){
+        //         System.out.println("Valor inserido incorreto, insira valores inteiros!");
+        //         scanner.next();
+        //         scanner.next();
+        //     }
+        // }
+
+        // if(inputValido){
+        //     Alimento alimento = new Alimento(alimentoX, alimentoY);
+        //     int valorRandom1 = 0;
+        //     int valorRandom2 = 0;
+        //     String direcao1 = null;
+        //     String direcao2 = null;
+        //     // variaveis de controle para evitar fluxo infinito
+        //     int maximoMovimentos = 250;
+        //     int qntdMovimentos = 0;
+
+        //     while ((!roboUm.encontrouAlimento(alimento) || !roboDois.encontrouAlimento(alimento)) && qntdMovimentos < maximoMovimentos) {
+        //         valorRandom1 = random.nextInt(4) + 1;
+        //         valorRandom2 = random.nextInt(4) + 1;
+
+        //         direcao1 = roboUm.mover(valorRandom1);
+        //         direcao2 = roboDois.mover(valorRandom2);
+
+        //         try {
+        //             roboUm.mover(direcao1);
+        //         } catch(MovimentoInvalidoException e){
+        //             System.out.println(e.getMessage());
+        //         }
+
+        //         try {
+        //             roboDois.mover(direcao2);
+        //         } catch(MovimentoInvalidoException e){
+        //             System.out.println(e.getMessage());
+        //         }
+
+        //         qntdMovimentos++;   
+        //     }
+               
+        //     if(roboUm.encontrouAlimento(alimento)){
+        //         System.out.println("O robô " + roboUm.getCor() + " encontrou o alimento!");
+        //     }
+        //     if(roboDois.encontrouAlimento(alimento)){
+        //         System.out.println("O robô " + roboDois.getCor() + " encontrou o alimento!");
+        //     }
+            
+        //     System.out.println("Movimentos Válidos de " + roboUm.getCor() + ": " + roboUm.getMovimentoValido());
+        //     System.out.println("Movimentos Inválidos de " + roboUm.getCor() + ": " + roboUm.getMovimentoInvalido());
+        //     System.out.println("Movimentos Válidos de " + roboDois.getCor() + ": " + roboDois.getMovimentoValido());
+        //     System.out.println("Movimentos Inválidos de " + roboDois.getCor() + ": " + roboDois.getMovimentoInvalido());
+
+        //     System.out.println("Total de movimentos executados: " + qntdMovimentos);
+        // }
+    }
+}
