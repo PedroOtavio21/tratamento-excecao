0e2c4b04dc08bc95d46564f56fef46934e00f55a
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-07-23 12:12:06 -0300

    Adicionando metodo adicionarObstaculo() em classe Tabuleiro

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -29,19 +29,26 @@ public class Tabuleiro {
         plano[robo.getX()][robo.getY()] = "R";
     }
 
+    public void adicionarObstaculo(Obstaculo obstaculo){
+        int[] coordenadas = obstaculo.getCoordenadas();
+        int x = coordenadas[0];
+        int y = coordenadas[1];
+
+        if(obstaculo instanceof Bomba){
+            plano[x][y] = "B";
+        } else if (obstaculo instanceof Rocha){
+            plano[x][y] = "R";
+        }
+    }
+
     public void mostrarTabuleiro() {
         for (int i = 0; i < TAMANHO; i++) {
             for (int j = 0; j < TAMANHO; j++) {
-
                 System.out.print(plano[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
-    }
-
-    public void retirarRobo(Robo robo) {
-
     }
 
 }
