6af3a25ee8fb0aa6c4996cce934cdb32c8f3026a
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-07-24 15:38:08 -0300

    Melhorando blocos relacionados a adicao de obstaculos no jogo, separando em dois blocos de loops

--- src/MainQuatro.java
+++ src/MainQuatro.java
@@ -7,7 +7,8 @@ public class MainQuatro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
+        
+        // Inicialização de variáveis do jogo
         int alimentoX = 0;
         int alimentoY = 0;
 
@@ -50,54 +51,54 @@ public class MainQuatro {
         }
         
         // TODO: Implementar a leitura e posicionamento de Bombas e Obstaculos
-        while(!inputValidoBomba && !inputValidoRocha){
-            if(qntdBombas > 0){
-                try{
-                    for(int i = 0; i < qntdBombas; i++){
+
+        // Loop para inserção de bombas
+        if(qntdBombas <= 0 || qntdBombas > 2){
+            System.out.println("Não será possível inseir bombas no jogo.");
+        } else {
+            for (int i = 0; i < qntdBombas; i++) {
+                while (!inputValidoBomba) {
+                    try {
                         System.out.println("Insira as coordenadas da bomba " + (i + 1));
                         bombaX = scanner.nextInt();
                         bombaY = scanner.nextInt();
-                        if(bombaX < 0 || bombaX >= 4 || bombaY < 0 || bombaY >= 4){
+                        if (bombaX < 0 || bombaX >= 4 || bombaY < 0 || bombaY >= 4) {
                             System.out.println("As coordenadas da bomba devem ser entre 0 e 3");
                             scanner.nextLine();
                         } else {
                             obstaculos.add(new Bomba(bombaX, bombaY));
                             inputValidoBomba = true;
                         }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Valor inserido incorreto, insira valores inteiros!");
+                        scanner.next();
                     }
-                } catch(InputMismatchException e){
-                    System.out.println("Valor inserido incorreto, insira valores inteiros!");
-                    scanner.next();
-                    scanner.next();
                 }
-            } else {
-                System.out.println("Não há bombas a serem inseridas no jogo.");
-                inputValidoBomba = true;
             }
+        }
 
-
-            if(qntdRochas > 0){
-                try{
-                    for(int i = 0; i < qntdRochas; i++){
+        // Loop para inserção de rochas
+        if(qntdRochas <= 0 || qntdRochas > 2){
+            System.out.println("Não será possível inseir rochas no jogo.");
+        } else {
+            for (int i = 0; i < qntdRochas; i++) {
+                while (!inputValidoRocha) {
+                    try {
                         System.out.println("Insira as coordenadas da rocha " + (i + 1));
                         rochaX = scanner.nextInt();
                         rochaY = scanner.nextInt();
-                        if(rochaX < 0 || rochaX >= 4 || rochaY < 0 || rochaY >= 4){
+                        if (rochaX < 0 || rochaX >= 4 || rochaY < 0 || rochaY >= 4) {
                             System.out.println("As coordenadas da rocha devem ser entre 0 e 3");
                             scanner.nextLine();
                         } else {
                             obstaculos.add(new Rocha(rochaX, rochaY));
                             inputValidoRocha = true;
                         }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Valor inserido incorreto, insira valores inteiros!");
+                        scanner.next();
                     }
-                } catch (InputMismatchException e){
-                    System.out.println("Valor inserido incorreto, insira valores inteiros!");
-                    scanner.next();
-                    scanner.next();
                 }
-            } else {
-                System.out.println("Não há rochas a serem inseridas no jogo.");
-                inputValidoRocha = true;
             }
         }
 
