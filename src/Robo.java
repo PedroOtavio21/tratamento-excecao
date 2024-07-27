9e7bc19c736daec108f2bee318035cf0b19ab4ae
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-07-27 12:31:57 -0300

    Removendo comentarios em excesso de classe Robo

--- src/Robo.java
+++ src/Robo.java
@@ -127,60 +127,10 @@ public class Robo {
             incrementaMovimentoInvalido();
             throw e; // Relança a mesma exceção capturada
         }    
-        // private int TAMANHO = 4;
-        // if (novoX < 0 || novoY < 0) {
-        //     incrementaMovimentoInvalido();
-        //     throw new MovimentoInvalidoException(
-        //             "Movimento inválido: posição negativa" + "(" + novoX + ", " + novoY + ")");
-        // }
-
-        // if (novoX >= TAMANHO || novoY >= TAMANHO) {
-        //     incrementaMovimentoInvalido();
-        //     throw new MovimentoInvalidoException(
-        //             "Movimento inválido: fora do limite" + "(" + novoX + ", " + novoY + ")");
-        // }
-
-        // this.x = novoX;
-        // this.y = novoY;
-        // incrementaMovimentoValido();
-        // System.out.println("Nova coordenada do robô " + this.getCor() + ": (" + this.getX() + "," + this.getY() + ").");
+       
     }
 
-    /*
-     * public void mover(String direcao) throws MovimentoInvalidoException {
-     * int novoX = this.getX();
-     * int novoY = this.getY();
-     * switch (direcao) {
-     * case "right":
-     * novoY++;
-     * break;
-     * case "left":
-     * novoY--;
-     * break;
-     * case "down":
-     * novoX--;
-     * break;
-     * case "up":
-     * novoX++;
-     * break;
-     * default:
-     * // Verifica se opção recebe um dos cases acima.
-     * throw new IllegalArgumentException("Direção aplicada inválida! " + direcao);
-     * }
-     * try {
-     * validarMovimento(novoX, novoY);
-     * this.setX(novoX);
-     * this.setY(novoY);
-     * incrementaMovimentoValido();
-     * System.out.println(
-     * "Nova coordenada do robô " + this.getCor() + ": (" + this.getY() + "," +
-     * this.getX() + ").");
-     * } catch (MovimentoInvalidoException e) {
-     * incrementaMovimentoInvalido();
-     * throw e; // Relanca a mesma excecao lancada
-     * }
-     * }
-     */
+ 
 
     // moverString() -> string
     public String mover(int opcao) {
@@ -204,75 +154,12 @@ public class Robo {
         return direcaoStr;
     }
     
-    /*
-     * public void incrementarX(int x) {
-     * this.x = (this.x + x + TAMANHO) % TAMANHO; // Adiciona TAMANHO para garantir
-     * que não seja negativo
-     * 
-     * }
-     * 
-     * public void incrementarY(int y) {
-     * this.y = (this.y + y + TAMANHO) % TAMANHO; // Adiciona TAMANHO para garantir
-     * que não seja negativo
-     * }
-     * 
-     * public void movendoRobo(String direcao) throws MovimentoInvalidoException {
-     * switch (direcao) {
-     * case "down":
-     * incrementarY(-1);
-     * break;
-     * case "up":
-     * incrementarY(1);
-     * break;
-     * case "left":
-     * incrementarX(-1);
-     * break;
-     * case "right":
-     * incrementarX(1);
-     * break;
-     * default:
-     * // Verifica se opção recebe um dos cases acima.
-     * throw new IllegalArgumentException("Direção aplicada inválida! " + direcao);
-     * }
-     * 
-     * 
-     * 
-     * System.out.println("Nova coordenada do robô " + this.getCor() + ": (" +
-     * this.getX() + "," + this.getY() + ").");
-     * }
-     */
-
-    // moverString() -> string
-    /*
-     * public String mover(int opcao) {
-     * String direcaoStr;
-     * switch (opcao) {
-     * case 1:
-     * direcaoStr = "up";
-     * break;
-     * case 2:
-     * direcaoStr = "down";
-     * break;
-     * case 3:
-     * direcaoStr = "right";
-     * break;
-     * case 4:
-     * direcaoStr = "left";
-     * break;
-     * default:
-     * // Verifica se opção recebe um dos cases acima.
-     * throw new IllegalArgumentException("Opção aplicada inválida! " + opcao);
-     * }
-     * return direcaoStr;
-     * }
-     */
-
     // encontrouAlimento() -> boolean
     public boolean encontrouAlimento(Alimento alimento) {
         return this.getX() == alimento.getX() && this.getY() == alimento.getY();
     }
 
-    // TODO: Implementar a volta de posicao do robô
+    // Volta de posicao do robô no tabuleiro
     public void voltarPosicaoAnterior(){
         this.setX(getXAnterior());
         this.setY(getYAnterior());
